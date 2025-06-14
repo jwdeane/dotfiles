5f14ff366044939910c6919bd614a45114634d85
James Deane <git@jwdeane.com>
2025-06-14 21:45:38 +1000

    Add scripts to set up config directories and post-config hooks

---
+++ home/.chezmoiscripts/run_onchange_before_setup-config-dirs.sh
@@ -0,0 +1,11 @@
+# create deno bin directory
+mkdir -p ~/.deno/bin
+
+# create golang bin directory
+mkdir -p ~/.go/bin
+
+# create volta directory
+mkdir -p ~/.config/volta/bin
+
+# create zsh config directories
+mkdir -p ~/.config/zsh/{completions,functions}
