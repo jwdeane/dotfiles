4432826f3febd6a6b8c5be43e4628e9c529e0784
James Deane <git@jwdeane.com>
2025-06-17 19:32:47 +1000

    Add dockutil to manage Dock layout

---
+++ home/.chezmoiscripts/run_onchange_after_configure-dock.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Configuring Dock"
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/Ghostty.app"
+dockutil --no-restart --add "/Applications/Zed.app"
+dockutil --no-restart --add "~/Downloads" --view grid --display folder
+
+# Dock: hide recent applications
+defaults write com.apple.dock show-recents -bool false
+
+killall Dock
