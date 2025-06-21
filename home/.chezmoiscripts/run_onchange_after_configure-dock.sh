4c1a3cc5a89f249b9cc58ff8437009f086e19c5b
James Deane <git@jwdeane.com>
2025-06-21 23:36:29 +1000

    add Obsidian to dock

--- home/.chezmoiscripts/run_onchange_after_configure-dock.sh
+++ home/.chezmoiscripts/run_onchange_after_configure-dock.sh
@@ -7,6 +7,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/Ghostty.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
 dockutil --no-restart --add "/Applications/Zed.app"
 dockutil --no-restart --add "~/Downloads" --view grid --display folder
 
